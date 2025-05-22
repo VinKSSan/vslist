7611c3f7a71e232b3836c79efca7d50fa5651bbb
VinKSSan <vinsan.ed@proton.me>
2025-05-22 10:26:17 +0000

    GET game by id

--- src/main/java/com/vinsaned/vslist/controller/GameController.java
+++ src/main/java/com/vinsaned/vslist/controller/GameController.java
@@ -1,10 +1,12 @@
 package com.vinsaned.vslist.controller;
 
+import com.vinsaned.vslist.dto.GameDto;
 import com.vinsaned.vslist.dto.GameMinDto;
 import com.vinsaned.vslist.etities.Game;
 import com.vinsaned.vslist.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,10 @@ public class GameController {
     @GetMapping
     public List<GameMinDto> findAll(){
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDto findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
 }
--- src/main/java/com/vinsaned/vslist/service/GameService.java
+++ src/main/java/com/vinsaned/vslist/service/GameService.java
@@ -1,8 +1,10 @@
 package com.vinsaned.vslist.service;
 
+import com.vinsaned.vslist.dto.GameDto;
 import com.vinsaned.vslist.dto.GameMinDto;
 import com.vinsaned.vslist.etities.Game;
 import com.vinsaned.vslist.repositories.GameRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +16,18 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional
+    public GameDto findById(Long id){
+        Game game = gameRepository.findById(id).get();
+        return new GameDto(game);
+    }
+
+    @Transactional
     public List<GameMinDto> findAll(){
         List <Game> games = gameRepository.findAll();
         return games.stream()
                 .map(GameMinDto::new)
                 .toList();
     }
+
 }
