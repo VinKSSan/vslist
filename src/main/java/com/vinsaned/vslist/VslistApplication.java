3726c1fe0271a81ef369459f4357570225f3fd35
VinKSSan <vinsan.ed@proton.me>
2025-05-22 00:11:06 +0000

    project created

---
+++ src/main/java/com/vinsaned/vslist/VslistApplication.java
@@ -0,0 +1,13 @@
+package com.vinsaned.vslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinsaned/vslist/VslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinsaned.vslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
